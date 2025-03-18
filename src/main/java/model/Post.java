857049752d0b808e161ed4ead721b39913d20334
JaeoneHeo <jaewonheo666@gmail.com>
2025-03-18 17:47:19 +0900

    [Feat] Post 엔티티 추가

---
+++ src/main/java/model/Post.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Post {
+    private int id;
+    private String title;
+    private String content;
+
+    public Post(int id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
