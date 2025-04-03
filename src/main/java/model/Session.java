11e2e6b20c60f3ca1e0fe4a4f0d4ddd640c4dc21
JaeoneHeo <jaewonheo666@gmail.com>
2025-04-03 22:49:19 +0900

    [Feat] Session 구현

---
+++ src/main/java/model/Session.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Session {
+    private Account loggedInAccount;
+
+    public Account getLoggedInAccount() {
+        return loggedInAccount;
+    }
+    public void setLoggedInAccount(Account loggedInAccount) {
+        this.loggedInAccount = loggedInAccount;
+    }
+}
